297372e2368cdfd18eb4a1f4ada1ca3d66c7b318
Elias Corsino <elias@codiflash.onmicrosoft.com>
2026-06-05 14:17:32 +0200

    revert: remove sitemap lastmod (hardcoded dates unreliable, ignored by Google for small sites)

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,32 +22,6 @@ export default defineConfig({
       customPages: [
         'https://chordsequence.com/songs/',
       ],
-      serialize: (item) => {
-        const LASTMOD: Record<string, string> = {
-          'https://chordsequence.com/': '2026-06-05',
-          'https://chordsequence.com/about/': '2026-06-05',
-          'https://chordsequence.com/about/elias-corsino/': '2026-01-01',
-          'https://chordsequence.com/contact/': '2025-04-01',
-          'https://chordsequence.com/learn/': '2026-06-05',
-          'https://chordsequence.com/learn/what-is-a-chord-progression/': '2025-05-10',
-          'https://chordsequence.com/learn/chord-progressions-for-beginners/': '2025-05-01',
-          'https://chordsequence.com/learn/jazz-chord-progressions/': '2026-06-01',
-          'https://chordsequence.com/learn/how-to-transpose-chords/': '2025-05-15',
-          'https://chordsequence.com/learn/music-theory-basics-for-songwriters/': '2025-05-25',
-          'https://chordsequence.com/learn/circle-of-fifths-explained/': '2025-05-20',
-          'https://chordsequence.com/learn/beginner-guitar-chords/': '2026-06-05',
-          'https://chordsequence.com/learn/easy-guitar-songs/': '2026-06-01',
-          'https://chordsequence.com/learn/chord-inversions/': '2026-06-05',
-          'https://chordsequence.com/progressions/': '2026-01-01',
-          'https://chordsequence.com/songs/': '2026-06-05',
-          'https://chordsequence.com/tools/': '2026-05-01',
-          'https://chordsequence.com/tools/chord-transposer/': '2026-05-01',
-          'https://chordsequence.com/tools/circle-of-fifths/': '2026-05-01',
-          'https://chordsequence.com/tools/key-detector/': '2026-05-01',
-        };
-        const lastmod = LASTMOD[item.url];
-        return lastmod ? { ...item, lastmod } : item;
-      },
     }),
   ],
 
