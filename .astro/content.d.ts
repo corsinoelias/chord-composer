9e64a29f8f14fd07b1f0b863f4bb3a68538f46ec
Elias Corsino <elias@codiflash.onmicrosoft.com>
2026-07-10 17:00:00 +0200

    Implementar nuevo post de  c major chord progressions

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -173,17 +173,7 @@ declare module 'astro:content' {
 	};
 
 	type DataEntryMap = {
-		"learn": Record<string, {
-  id: string;
-  render(): Render[".md"];
-  slug: string;
-  body: string;
-  collection: "learn";
-  data: InferEntrySchema<"learn">;
-  rendered?: RenderedContent;
-  filePath?: string;
-}>;
-
+		
 	};
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
@@ -215,6 +205,6 @@ declare module 'astro:content' {
 		LiveContentConfig['collections'][C]['loader']
 	>;
 
-	export type ContentConfig = typeof import("./../src/content/config.js");
+	export type ContentConfig = never;
 	export type LiveContentConfig = never;
 }
